d811b76b73cbcd57bafa029e8bcd5f4e93ed0ff7
jeffrey-jolly <jeffryjolly@gmail.com>
2021-08-04 18:59:12 +0530

    main committ

--- src/db.js
+++ src/db.js
@@ -1,21 +1,20 @@
 var sqlite3 = require('sqlite3').verbose();
 // var md5 = require('md5');
 
-const DB_SOURCE = "db.sqlite";
+const DB_SOURCE = "movie_db.sqlite";
 
-let db = new sqlite3.Database(DB_SOURCE,(err)=>{
-    if(err){
+let db = new sqlite3.Database(DB_SOURCE, (err) => {
+    if (err) {
         console.log(err.message);
         throw err;
-    }
-    else{
+    } else {
         console.log('SQLite db Connected');
         db.run("CREATE TABLE MOVIES (Movie_Name TEXT, Lead_Actor TEXT, Lead_Actress TEXT, Director TEXT, Year_of_Release INTEGER)",
-        (err)=>{
-            if(err){
-                console.log(err.message);
-            }
-        });
+            (err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+            });
     }
 });
 
